7abe3f3333fe0b691f93041347db0d186c0d1c2d
hanavinemufan <hanavinemu@gmail.com>
2025-05-14 21:53:06 +0900

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "25312a97faf82b3be00f975dcabb85c3",
+const RESOURCES = {"flutter_bootstrap.js": "a276fcd6e2dc85aa1331f5192ccb3443",
 "version.json": "beab5fbfcd72f6b88b49534eda6d7095",
-"index.html": "dd3da6f2a06f205f536bc2037421f429",
-"/": "dd3da6f2a06f205f536bc2037421f429",
-"main.dart.js": "c96d512bd67eeeb14b5bde4e6b8151ed",
+"index.html": "9b899b518ebad784899c622c85313c5b",
+"/": "9b899b518ebad784899c622c85313c5b",
+"main.dart.js": "845aa0a0b0bd0e3da5cdec1ad1860cbc",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "7d6c712a62d2413e2a8f37c9fee5bc70",
 "icons/Icon-192.png": "75a7d977daf01db316b0a39bca27207b",
